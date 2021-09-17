e36cff744a54fbe01170a6217bc657da7433e5a2
jeremylhour <jeremy.lhour@insee.fr>
2021-09-17 10:26:11 +0000

    init nlp

---
+++ traitement_langage_naturel/setup_nlp.sh
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------
+# Script de téléchargement des paquets pour traitement du langage naturel
+#
+# 17 septembre 2021
+#
+# @author : jeremylhour
+# ------------------------------------------------------------------
+echo TELECHARGEMENT DES PAQUETS TRAITEMENT LANGAGE NATUREL
+
+echo INSTALLATION DE FASTTEXT ET TELECHARGEMENT DES VECTEURS FRANCAIS
+git clone https://github.com/facebookresearch/fastText.git
+cd fastText
+sudo pip install .
+
+echo TELECHARGEMENT DES VECTEURS DE MOT ET REDUCTION DE LA DIMENSION
+./download_model.py fr
+./reduce_model.py cc.fr.300.bin 100
+cd ..
