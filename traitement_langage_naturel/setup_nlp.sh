4fdc2079d790e3e20df5bed97745ca35c093060e
zctxti <jeremy.lhour@insee.fr>
2021-12-03 11:05:41 +0000

    graphiques word vectors

--- traitement_langage_naturel/setup_nlp.sh
+++ traitement_langage_naturel/setup_nlp.sh
@@ -7,16 +7,12 @@
 # ------------------------------------------------------------------
 echo TELECHARGEMENT DES PAQUETS TRAITEMENT LANGAGE NATUREL
 
-pip install unidecode
+pip install -r requirements.txt
 
 echo INSTALLATION DE FASTTEXT ET TELECHARGEMENT DES VECTEURS FRANCAIS
 git clone https://github.com/facebookresearch/fastText.git
 cd fastText
-sudo pip install .
-
-echo TELECHARGEMENT DES VECTEURS DE MOT ET REDUCTION DE LA DIMENSION
 ./download_model.py fr
-./reduce_model.py cc.fr.300.bin 100
 cd ..
 
 echo TELECHARGEMENT DES DONNEES
